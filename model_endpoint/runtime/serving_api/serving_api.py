4b81f416f37ec811687d3ad7b9ba8079378314ee
Demir Catovic <dcatovic@amazon.com>
2022-11-17 13:15:01 +0100

    added serving_api.py as well as requirements.txt

---
+++ model_endpoint/runtime/serving_api/serving_api.py
@@ -0,0 +1,48 @@
+import json
+import logging
+from fastapi import FastAPI
+from mangum import Mangum
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+app = FastAPI(root_path="/prod")
+
+@app.get("/")
+async def root() -> dict:
+    """
+    **Dummy endpoint that returns 'hello world' example.**
+
+    ```
+    Returns:
+        dict: 'hello world' message.
+    ```
+    """
+    return {
+        "message": "Hello World"
+    }
+
+@app.get("/question")
+async def get_answer(question: str, context: str) -> dict:
+    """
+    **Endpoint implementing the question-answering logic.**
+
+    ```
+    Args:
+        question (str): Question to be answered. Answer should be included in 'context'.
+        context (str): Context that should contain the information necessary to answer 'question'.
+    Returns:
+        dict: Dictionary containing the answer to the question along with some metadata.
+    ```
+    """
+    from custom_lambda_utils.scripts.inference import question_answerer
+    return question_answerer(question=question, context=context)
+
+def lambda_handler(event, context):
+    logger.info(json.dumps(event))
+
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context) # Call the instance with the event arguments
+
+    logger.info(json.dumps(response))
+    return response
