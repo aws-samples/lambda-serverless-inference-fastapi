7fe4cbd340c0eb252365b898b86c79199995da1c
Demir Catovic <dcatovic@amazon.com>
2022-11-17 11:10:54 +0100

    added some files

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from fastapi_model_serving.fastapi_model_serving_stack import FastapiModelServingStack
+
+
+app = cdk.App()
+
+FastapiModelServingStack(
+    app,
+    "FastapiModelServingStack",
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+                        region=app.node.try_get_context("DEPLOYMENT_REGION")),
+    )
+
+app.synth()
---
+++ fastapi_model_serving/fastapi_model_serving_stack.py
@@ -0,0 +1,38 @@
+import os
+from aws_cdk import (
+    Size,
+    Duration,
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
+)
+from constructs import Construct
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+class FastapiModelServingStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        fastapi_model_endpoint_lambda = _lambda.DockerImageFunction(
+            self,
+            "fastapi_model_serving_endpoint",
+            function_name="fastapi_model_serving_endpoint_docker",
+            architecture=_lambda.Architecture.ARM_64,
+            # architecture=_lambda.Architecture.X86_64,
+            code=_lambda.DockerImageCode.from_image_asset(
+                os.path.join(DIR_PATH, "..","model_endpoint", "docker")
+            ),
+            timeout=Duration.seconds(60),
+            ephemeral_storage_size=Size.mebibytes(4096),
+            memory_size=2048
+            )
+
+        apigateway.LambdaRestApi(
+            self,
+            "docker_model_serving_endpoint",
+            handler=fastapi_model_endpoint_lambda,
+            proxy=True
+            )
+        
